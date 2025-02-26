9880fdf7994fe7dde4a3a5c2249c30703204a68a
karan0615 <karansinghipg2009@gmail.com>
2025-02-26 23:40:45 +0530

    Fixed database connection issue

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// Create a new Sequelize instance with explicit parameters
+// Create a new Sequelize instance
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306, // Default MySQL port
   dialect: "mysql",
+  port: process.env.DB_PORT || 3306,
   logging: false, // Set to true for debugging queries
 });
 
